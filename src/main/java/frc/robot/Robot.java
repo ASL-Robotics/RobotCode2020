6149c05ab03c6463b083b2c552b45d7605d5dc3a
Lorenzo <awesomelorenzokwenda@gmail.com>
2020-01-18 09:53:13 +0000

    intake branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,8 +16,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
 
   /**
@@ -46,7 +42,6 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
   }
 
   /**
@@ -65,12 +60,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
   /**
@@ -86,9 +75,6 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
   }
 
   /**
@@ -96,19 +82,17 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    m_robotContainer.getDrivetrain().arcadeDrive(m_robotContainer.driverController.getRawAxis(Constants.FORWARD_AXIS_LEFT),
-          m_robotContainer.driverController.getRawAxis(Constants.FORWARD_AXIS_RIGHT));
   }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
+    // Cancels all running commands at the start of test mode.  
   }
 
   /**
    * This function is called periodically during test mode.
    */
+
   @Override
   public void testPeriodic() {
   }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.auto.routines.TestAutoCommandGroup;
 import frc.robot.subsystems.*;
 
 import static frc.robot.Constants.*;
@@ -19,62 +18,9 @@ import static frc.robot.Constants.*;
 public class RobotContainer {
 
     // SUBSYSTEMS
-    private final Drivetrain DRIVETRAIN = new Drivetrain();
-    private final Climber CLIMBER = new Climber();
-    private final Shooter SHOOTER = new Shooter();
     private final Intake INTAKE = new Intake();
 
-    // CLIMBER COMMANDS
-
-    private final StartEndCommand climb = new StartEndCommand(
-            //runnable on init
-            () -> CLIMBER.reverseHookPiston(),
-            //runnable on end
-            () -> CLIMBER.stopHookPiston(),
-            CLIMBER
-    );
-
-    private final StartEndCommand raiseHooks = new StartEndCommand(
-            //runnable on init
-            () -> CLIMBER.raiseHooks(),
-            //runnable on end
-            () -> CLIMBER.stopHookPiston(),
-            CLIMBER
-    );
-
-    private final StartEndCommand raiseClimbPistons = new StartEndCommand(
-            //runnable on init
-            () -> CLIMBER.raiseClimber(),
-            //runnable on end
-            () -> CLIMBER.stopRaisePiston(),
-            CLIMBER
-    );
-
-    private final StartEndCommand lowerClimbPistons = new StartEndCommand(
-            //runnable on init
-            () -> CLIMBER.reverseRaisePiston(),
-            //runnable on end
-            () -> CLIMBER.stopRaisePiston(),
-            CLIMBER
-    );
-
-    // SHOOTER COMMANDS
-    
-    private final StartEndCommand shootAtSpeed = new StartEndCommand(
-        
-    // TODO: change set speed parameter to variable if vision processing works.
-        
-        //Runnable on initialise
-        () -> SHOOTER.setSpeed(1),
-        //Runnable on end
-        () -> SHOOTER.stopShooter(),
-        SHOOTER
-
-
-    );
-
     // INTAKE COMMANDS
-
     private final StartEndCommand deployIntake = new StartEndCommand(
         () -> INTAKE.deploySpeed(DEPLOY_INTAKE_SPEED),
         () -> INTAKE.deploySpeed(0),
@@ -116,20 +62,11 @@ public class RobotContainer {
   
     // MAKE A NEW JOYSTICK
 
-    public final Joystick driverController = new Joystick(DRIVER_CONTROLLER), opController = new Joystick(OPERATOR_CONTROLLER);
+    public final Joystick opController = new Joystick(OPERATOR_CONTROLLER);
   
     // CONFIG BUTTON BINDINGS (See constants.java to change specific ports etc.)
 
     // CLIMB BUTTONS
-
-    private final JoystickButton climbButton = new JoystickButton(opController, CLIMB_BUTTON),
-                                 raiseHooksButton = new JoystickButton(opController, RAISE_HOOKS_BUTTON),
-                                 raiseClimbPistonsButton = new JoystickButton(opController, RAISE_CLIMB_PISTONS_BUTTON),
-                                 lowerClimbPistonsButton = new JoystickButton(opController, LOWER_CLIMB_PISTONS_BUTTON);
-
-    // SHOOT BUTTON (TOGGLEABLE)
-
-    private final JoystickButton shootButton = new JoystickButton(opController, SHOOT_BUTTON);
 
     // INTAKE BUTTONS
 
@@ -156,15 +93,6 @@ public class RobotContainer {
      * Config button actions: it changes what does each button do. Don't touch this to change bindings
      */
     private void configureButtonActions() {
-        // CLIMB BUTTONS
-        climbButton.whenPressed(climb.withTimeout(8));
-        raiseHooksButton.whenPressed(raiseHooks.withTimeout(6));
-        raiseClimbPistonsButton.whenPressed(raiseClimbPistons.withTimeout(2));
-        lowerClimbPistonsButton.whenPressed(lowerClimbPistons.withTimeout(2));
-
-        // SHOOT BUTTONS
-        // TODO - this might not work. Check (I think it does... see Button.class)
-        shootButton.toggleWhenPressed(shootAtSpeed);
 
         // TODO- add buttons for intake and change climb buttons
 
@@ -183,10 +111,6 @@ public class RobotContainer {
 
     }
 
-    public Drivetrain getDrivetrain() {
-        return this.DRIVETRAIN;
-    }
-
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
@@ -195,7 +119,7 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new TestAutoCommandGroup(DRIVETRAIN);
+        return null;
 
     }
 
